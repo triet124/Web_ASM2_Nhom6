d3e5478f43f9b095dc44230b4d5a1c0146944132
nghiaDEV <149445948+nghiamhhpc05062@users.noreply.github.com>
2024-06-05 17:27:09 +0700

    create

---
+++ Web_ASM_Nhom6/Web_ASM_Nhom6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_ASM_Nhom6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
